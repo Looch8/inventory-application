b17d114d0222badcdcd9b240c7dfae95456cd6d8
Luke Wheldale <lukewheldale@gmail.com>
2024-10-17 09:42:48 +1030

    set up initial folders and files

--- app.js
+++ app.js
@@ -1,39 +1,12 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
+const express = require("express")
+const app = express()
 
-const path = require("node:path");
+app.set('view engine', 'ejs')
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 
-const gameRoute = require("./routes/gameRoute");
-app.use("/", gameRoute);
+app.get('/', (req, res) => {
+	res.render('index',{foo: 'Foo'})
+	
+})
 
-app.get("/", (req, res) => res.render("index"));
-
-app.listen(process.env.PORT || 4001, () => {
-	console.log(`listening on port ${process.env.PORT}!`);
-});
-
-// *** GAME MANAGEMENT APP ***
-
-// Database tables and fields I may need
-
-// Users
-// - id
-// - username
-// - password
-// - email
-
-// Games
-// - id
-// - title
-// - description
-// - genre
-// - platform
-// - developer
-
-// Genres
-// - id
-// - name
+app.listen(3000)
--- routes/gameRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const gameController = require("../controllers/gameController");
-
-router.get("/game", gameController.getAllGames);
-
-module.exports = router;
